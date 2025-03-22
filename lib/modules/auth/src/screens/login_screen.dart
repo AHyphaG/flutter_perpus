972a5641a5568cbc45c72f965b23611277e8c1fa
Ahmad Mulfasa <dairobi456.jkt@gmail.com>
2025-03-22 13:40:05 +0700

    Route Login-Home

--- lib/modules/auth/src/screens/login_screen.dart
+++ lib/modules/auth/src/screens/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_perpus/modules/home/src/screens/home_screen.dart';
 import '../blocs/auth_bloc.dart';
 import '../blocs/auth_event.dart';
 import '../blocs/auth_state.dart';
@@ -30,7 +31,7 @@ class _LoginScreenState extends State<LoginScreen> {
         listener: (context, state) {
           if (state is AuthSuccess) {
             ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Login berhasil!")));
-            // Arahkan ke halaman berikutnya
+            Navigator.pushReplacementNamed(context, HomeScreen.routeName);
           } else if (state is AuthFailure) {
             ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(state.message)));
           }
@@ -43,9 +44,25 @@ class _LoginScreenState extends State<LoginScreen> {
           return Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                TextField(controller: emailController, decoration: InputDecoration(labelText: 'Username')),
-                TextField(controller: passwordController, decoration: InputDecoration(labelText: 'Password'), obscureText: true),
+                TextField(
+                  controller: emailController, 
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                    icon: Icon(Icons.person),
+                    border: OutlineInputBorder()
+                    ),
+                  ),
+                SizedBox(height: 20,),
+                TextField(
+                  controller: passwordController, 
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    icon: Icon(Icons.lock),
+                    border: OutlineInputBorder()
+                    ), 
+                  obscureText: true),
                 SizedBox(height: 20),
                 ElevatedButton(onPressed: _onLoginPressed, child: Text('Login')),
               ],
